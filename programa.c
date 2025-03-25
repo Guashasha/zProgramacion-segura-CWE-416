8dde9790ac6ab154cee8b0c64b0019a0b4cb9e20
Guashasha <92059195+Guashasha@users.noreply.github.com>
2025-03-25 13:56:20 -0600

    Add files via upload

---
+++ programa.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct auth {
+  char name[32];
+  int auth;
+};
+
+struct auth *auth;
+char *service;
+
+int main(int argc, char *argv[]) {
+  char line[128];
+  printf("Ingresa [auth admin] o reset o [service ftp] o login.\n");
+
+  while (1) {
+    printf("[ auth = %p, service = %p ]\n", auth, service);
+
+    if (fgets(line, sizeof(line), stdin) == NULL) {
+      break;
+    }
+
+    if (strncmp(line, "auth ", 5) == 0) {
+      auth = malloc(sizeof(auth));
+      memset(auth, 0, sizeof(auth));
+
+      if (strlen(line + 5) < 31) {
+        strcpy(auth->name, line + 5);
+      }
+    } else if (strncmp(line, "reset", 5) == 0) {
+      free(auth); // auth = NULL;
+    } else if (strncmp(line, "service", 7) == 0) {
+      service = strdup(line + 7);
+    } else if (strncmp(line, "login", 5) == 0) {
+      if (auth->auth) {
+        printf("Bienvenido. Lo lograste!\n");
+      } else {
+        printf("Por favor, ingrese su nombre en la opcion auth\n");
+      }
+    }
+  }
+
+  return 0;
+}
